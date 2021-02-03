4091d67280769d359c001bf12850662cd1be67ee
zhaocheng.zhai <zzcheng.me@gmail.com>
2021-02-03 16:11:29 +0800

    update: delete code

--- src/pages/TableList/index.tsx
+++ src/pages/TableList/index.tsx
@@ -1,60 +1,61 @@
 import { PlusOutlined } from '@ant-design/icons';
-import { Button, message, Input, Drawer } from 'antd';
+// eslint-disable-line no-unused-vars
+import { Button, message, Input } from 'antd';
 import React, { useState, useRef } from 'react';
 import { useIntl, FormattedMessage } from 'umi';
 import { PageContainer, FooterToolbar } from '@ant-design/pro-layout';
 import type { ProColumns, ActionType } from '@ant-design/pro-table';
 import ProTable from '@ant-design/pro-table';
-import { ModalForm, ProFormText, ProFormTextArea } from '@ant-design/pro-form';
-import type { ProDescriptionsItemProps } from '@ant-design/pro-descriptions';
-import ProDescriptions from '@ant-design/pro-descriptions';
-import type { FormValueType } from './components/UpdateForm';
-import UpdateForm from './components/UpdateForm';
+// import { ModalForm, ProFormText, ProFormTextArea } from '@ant-design/pro-form';
+// import type { ProDescriptionsItemProps } from '@ant-design/pro-descriptions';
+// import ProDescriptions from '@ant-design/pro-descriptions';
+// import type { FormValueType } from './components/UpdateForm';
+// import UpdateForm from './components/UpdateForm';
 import type { TableListItem } from './data.d';
-import { queryRule, updateRule, addRule, removeRule } from './service';
+import { queryRule, removeRule } from './service';
 
 /**
  * 添加节点
  *
  * @param fields
  */
-const handleAdd = async (fields: TableListItem) => {
-  const hide = message.loading('正在添加');
-  try {
-    await addRule({ ...fields });
-    hide();
-    message.success('添加成功');
-    return true;
-  } catch (error) {
-    hide();
-    message.error('添加失败请重试！');
-    return false;
-  }
-};
+// const handleAdd = async (fields: TableListItem) => {
+//   const hide = message.loading('正在添加');
+//   try {
+//     await addRule({ ...fields });
+//     hide();
+//     message.success('添加成功');
+//     return true;
+//   } catch (error) {
+//     hide();
+//     message.error('添加失败请重试！');
+//     return false;
+//   }
+// };
 
 /**
  * 更新节点
  *
  * @param fields
  */
-const handleUpdate = async (fields: FormValueType) => {
-  const hide = message.loading('正在配置');
-  try {
-    await updateRule({
-      name: fields.name,
-      desc: fields.desc,
-      key: fields.key,
-    });
-    hide();
+// const handleUpdate = async (fields: FormValueType) => {
+//   const hide = message.loading('正在配置');
+//   try {
+//     await updateRule({
+//       name: fields.name,
+//       desc: fields.desc,
+//       key: fields.key,
+//     });
+//     hide();
 
-    message.success('配置成功');
-    return true;
-  } catch (error) {
-    hide();
-    message.error('配置失败请重试！');
-    return false;
-  }
-};
+//     message.success('配置成功');
+//     return true;
+//   } catch (error) {
+//     hide();
+//     message.error('配置失败请重试！');
+//     return false;
+//   }
+// };
 
 /**
  * 删除节点
@@ -80,14 +81,14 @@ const handleRemove = async (selectedRows: TableListItem[]) => {
 
 const TableList: React.FC = () => {
   /** 新建窗口的弹窗 */
-  const [createModalVisible, handleModalVisible] = useState<boolean>(false);
+  // const [createModalVisible, handleModalVisible] = useState<boolean>(false);
   /** 分布更新窗口的弹窗 */
-  const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
+  // const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
 
-  const [showDetail, setShowDetail] = useState<boolean>(false);
+  // const [showDetail, setShowDetail] = useState<boolean>(false);
 
   const actionRef = useRef<ActionType>();
-  const [currentRow, setCurrentRow] = useState<TableListItem>();
+  // const [currentRow, setCurrentRow] = useState<TableListItem>();
   const [selectedRowsState, setSelectedRows] = useState<TableListItem[]>([]);
 
   /** 国际化配置 */
@@ -103,12 +104,12 @@ const TableList: React.FC = () => {
       ),
       dataIndex: 'name',
       tip: '规则名称是唯一的 key',
-      render: (dom, entity) => {
+      render: (dom) => {
         return (
           <a
             onClick={() => {
-              setCurrentRow(entity);
-              setShowDetail(true);
+              // setCurrentRow(entity);
+              // setShowDetail(true);
             }}
           >
             {dom}
@@ -193,12 +194,12 @@ const TableList: React.FC = () => {
       title: <FormattedMessage id="pages.searchTable.titleOption" defaultMessage="操作" />,
       dataIndex: 'option',
       valueType: 'option',
-      render: (_, record) => [
+      render: () => [
         <a
           key="config"
           onClick={() => {
-            handleUpdateModalVisible(true);
-            setCurrentRow(record);
+            // handleUpdateModalVisible(true);
+            // setCurrentRow(record);
           }}
         >
           <FormattedMessage id="pages.searchTable.config" defaultMessage="配置" />
@@ -227,7 +228,7 @@ const TableList: React.FC = () => {
             type="primary"
             key="primary"
             onClick={() => {
-              handleModalVisible(true);
+              // handleModalVisible(true);
             }}
           >
             <PlusOutlined /> <FormattedMessage id="pages.searchTable.new" defaultMessage="新建" />
@@ -268,13 +269,15 @@ const TableList: React.FC = () => {
             }}
           >
             <FormattedMessage id="pages.searchTable.batchDeletion" defaultMessage="批量删除" />
+            123123
           </Button>
           <Button type="primary">
             <FormattedMessage id="pages.searchTable.batchApproval" defaultMessage="批量审批" />
+            1111
           </Button>
         </FooterToolbar>
       )}
-      <ModalForm
+      {/* <ModalForm
         title={intl.formatMessage({
           id: 'pages.searchTable.createForm.newRule',
           defaultMessage: '新建规则',
@@ -308,8 +311,8 @@ const TableList: React.FC = () => {
           name="name"
         />
         <ProFormTextArea width="md" name="desc" />
-      </ModalForm>
-      <UpdateForm
+      </ModalForm> */}
+      {/* <UpdateForm
         onSubmit={async (value) => {
           const success = await handleUpdate(value);
           if (success) {
@@ -326,9 +329,9 @@ const TableList: React.FC = () => {
         }}
         updateModalVisible={updateModalVisible}
         values={currentRow || {}}
-      />
+      /> */}
 
-      <Drawer
+      {/* <Drawer
         width={600}
         visible={showDetail}
         onClose={() => {
@@ -350,7 +353,7 @@ const TableList: React.FC = () => {
             columns={columns as ProDescriptionsItemProps<TableListItem>[]}
           />
         )}
-      </Drawer>
+      </Drawer> */}
     </PageContainer>
   );
 };
